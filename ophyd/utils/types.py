ff071af0f9af592cbde1fcb04fdf1a443ad71716
Sean Dawson <dawsons@ansto.gov.au>
2025-05-14 10:48:38 +1000

    Use older style typing that tox can handle

--- ophyd/utils/types.py
+++ ophyd/utils/types.py
@@ -1,14 +1,14 @@
-from typing import Literal, TypedDict
+from typing import Literal, TypedDict, List, Tuple
 
 
 class Hints(TypedDict, total=False):
     """A dictionary of optional hints for visualization"""
 
     #: A list of the interesting fields to plot
-    fields: list[str]
+    fields: List[str]
     #: Partition fields (and their stream name) into dimensions for plotting
     #:
     #: ``'dimensions': [(fields, stream_name), (fields, stream_name), ...]``
-    dimensions: list[tuple[list[str], str]]
+    dimensions: List[Tuple[List[str], str]]
     #: Include this if scan data is sampled on a regular rectangular grid
     gridding: Literal["rectilinear", "rectilinear_nonsequential"]
